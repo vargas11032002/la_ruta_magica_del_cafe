8092ef0a47c5036baf7177a7a5f4fc3ce8de5c2f
jhon vargas <120755975+vargas11032002@users.noreply.github.com>
2023-12-08 10:03:31 -0500

    Update articulos.js

--- frontend/src/articulos.js
+++ frontend/src/articulos.js
@@ -2,23 +2,23 @@ import Cafe from "./assets/images/cafe.jpg";
 import Arequipe from "./assets/images/arequipe.jpg";
 import Vinos from "./assets/images/vino.jpg";
 
-const arrayProductss = [
+const arrayProducts = [
   {
-    id: "price_1OL4i6FqzLAtNtxMydPC8u3g",
+    id: "price_1O5HNaFqzLAtNtxMfPdL1adn",
     name: "Cafe artesanal",
     description: "Disfruta de nuestro delicioso café artesanal, cuidadosamente seleccionado y preparado para ofrecerte una experiencia única.",
     price: 5000,
     image: Cafe,
   },
   {
-    id: "price_1OL4k1FqzLAtNtxMNKgbtOdh",
+    id: "price_1O5IQSFqzLAtNtxMHh28bO9T",
     name: "Arequipe",
     description: "Prueba nuestro arequipe casero, hecho con ingredientes de alta calidad que le dan un sabor irresistible.",
     price: 5000,
     image: Arequipe,
   },
   {
-    id: "price_1OL4ktFqzLAtNtxMC5FiFS41",
+    id: "price_1O5IRkFqzLAtNtxMlQ63WLNf",
     name: "Vinos de café",
     description: "Descubre la fusión perfecta entre el aroma del café y la exquisitez de un buen vino. Nuestros vinos de café te sorprenderán.",
     price: 10000,
@@ -26,7 +26,7 @@ const arrayProductss = [
   },
 ];
 
-function getProductDataa(id) {
+function getProductData(id) {
   let productData = arrayProducts.find((product) => product.id === id);
 
   if (productData === undefined) {
@@ -37,4 +37,5 @@ function getProductDataa(id) {
   return productData;
 }
 
-export { arrayProductss, getProductDataa };
+export { arrayProducts, getProductData };
+

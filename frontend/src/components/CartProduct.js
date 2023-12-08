05c6ea7a271f46b169c7753a9530981299a08dbe
jhon vargas <120755975+vargas11032002@users.noreply.github.com>
2023-12-08 10:01:56 -0500

    Update CartProduct.js

--- frontend/src/components/CartProduct.js
+++ frontend/src/components/CartProduct.js
@@ -1,24 +1,23 @@
 import { Cart } from "../Cart";
 import { useContext } from "react";
 import { getProductData } from "../Products";
-import { getProductDataa } from "../articulos"; // Add the import for articulos
 
 function CartProduct(props) {
   const cart = useContext(Cart);
   const id = props.id;
   const quantity = props.quantity;
-
-  // Use getProductData or getProductDataa based on the source of the product
-  const productData = getProductData(id) || getProductDataa(id);
+  const productData = getProductData(id);
 
   return (
     <div>
-      <h5 className="" style={{ color: "purple" }}>
+      <h5 className="" style={{color: "purple"}}>
         {productData.name}
       </h5>
-      <h6>cantidad: {quantity}</h6>
+      <h6>
+        cantidad: {quantity}
+      </h6>
       <p>
-        $ {(quantity * productData.price).toFixed(2).toString().replace(".", ",").replace(/,00/, "")}
+      $ {(quantity * productData.price).toFixed(2).toString().replace(".", ",").replace(/,00/,'')}
       </p>
       <button className="btn btn-warning" onClick={() => cart.deleteItem(id)}>
         Remover reserva
@@ -28,4 +27,4 @@ function CartProduct(props) {
   );
 }
 
-export default CartProduct;
+export default CartProduct
